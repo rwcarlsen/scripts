cad326f7d45e17142428eb581bb61c515102a3de
Robert Carlsen <rwcarlsen@gmail.com>
2012-06-10 14:12:04 -0500

    added another entry to backup script and made it executable.

--- util/backup.py
+++ util/backup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
-
 import pyrsync
 
-bk = pyrsync.Bkup('incr', '/media/spare/backups/Pictures')
+bk = pyrsync.Bkup('incr', '/media/spare/backups/incr/Pictures')
 bk.addsrc('/home/robert/Pictures')
 
-bk = pyrsync.Bkup('incr', '/media/spare/backups/repos')
+bk = pyrsync.Bkup('mirror', '/media/spare/backups/mirror/bit-wallet')
+bk.addsrc('/home/robert/.bitcoin')
+
+bk = pyrsync.Bkup('incr', '/media/spare/backups/incr/repos')
 bk.addsrc('/home/robert/repos')
 
-bk = pyrsync.Bkup('mirror', '/media/spare/backups')
+bk = pyrsync.Bkup('mirror', '/Volumes/Desktops/Robert/backups/repos')
 bk.dstssh('family@my1.homeftp.net')
 bk.addsrc('/home/robert/repos')
 
