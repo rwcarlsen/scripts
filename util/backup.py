bb1b13290da75f49cdefa45515d505bc4352b60e
Robert Carlsen <rwcarlsen@gmail.com>
2012-06-10 15:15:33 -0500

    added entry for git folder to backups.

--- util/backup.py
+++ util/backup.py
@@ -4,11 +4,14 @@ import pyrsync
 bk = pyrsync.Bkup('incr', '/media/spare/backups/incr/Pictures')
 bk.addsrc('/home/robert/Pictures')
 
-bk = pyrsync.Bkup('mirror', '/media/spare/backups/mirror/bit-wallet')
-bk.addsrc('/home/robert/.bitcoin')
-
 bk = pyrsync.Bkup('incr', '/media/spare/backups/incr/repos')
 bk.addsrc('/home/robert/repos')
+
+bk = pyrsync.Bkup('mirror', '/media/spare/backups/mirror/git')
+bk.addsrc('/home/robert/git')
+
+bk = pyrsync.Bkup('mirror', '/media/spare/backups/mirror/bit-wallet')
+bk.addsrc('/home/robert/.bitcoin')
 
 bk = pyrsync.Bkup('mirror', '/Volumes/Desktops/Robert/backups/repos')
 bk.dstssh('family@my1.homeftp.net')
