fa52d995e04638c5332a52ac588dd3a18527231c
Robert Carlsen <rwcarlsen@gmail.com>
2013-07-16 13:38:57 -0500

    tidied pic backup entries. added entries for docs and git repos and vids

--- backup/backup.sh
+++ backup/backup.sh
@@ -9,12 +9,18 @@ SERV=robert@my1.homeftp.net
 SERVDIR=/volume1/homes/robert/backups
 LOCALDIR=/media/spare/backups
 
-PICLIB=$HOME/Pictures/rwc-piclib
+PICLIB=$HOME/pics/rwc-piclib
+backs[picdata-nas]="--ignore-existing $PICLIB -e ssh $SERV:$SERVDIR/"
+backs[picmeta-nas]="$PICLIB/metadata -e ssh $SERV:$SERVDIR/rwc-piclib/"
+backs[picdata-local]="--ignore-existing $PICLIB $LOCALDIR/"
+backs[picmeta-local]="$PICLIB/metadata $LOCALDIR/rwc-piclib/"
 
-backs[picdata-nas]="--ignore-existing $PICLIB/ -e ssh $SERV:$SERVDIR/rwc-piclib"
-backs[picmeta-nas]="$PICLIB/metadata/ -e ssh $SERV:$SERVDIR/rwc-piclib/metadata"
-backs[picdata-local]="--ignore-existing $PICLIB/ $LOCALDIR/rwc-piclib"
-backs[picmeta-local]="$PICLIB/metadata/ $LOCALDIR/rwc-piclib/metadata"
+backs[git-local]="$HOME/git $LOCALDIR/"
+
+backs[docs-local]="$HOME/docs $LOCALDIR/"
+backs[docs-nas]="$HOME/docs $LOCALDIR/"
+
+backs[vids-local]="--ignore-existing $HOME/vids $LOCALDIR/"
 
 ############# end custom src-dst backup entries ##################
 
